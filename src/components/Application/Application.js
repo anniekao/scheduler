df7040520e180322dc015da5af6fd73ebe2ee57f
Annie <anniekao@posteo.net>
2019-09-03 14:31:01 -0400

    add: state object

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -61,12 +61,17 @@ const appointments = [
 ];
 
 
-
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: []
+  });
 
-  const schedules = appointments.map((appointment) => 
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
+
+   const schedules = appointments.map((appointment) => 
     <Appointment key={appointment.id === appointment.length + 1 ? "last" : appointment.id} {...appointment} />
   );
 
@@ -88,7 +93,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
